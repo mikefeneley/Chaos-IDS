11aae6cd3f326de6e0d820565c31e1d46dd2b57b
Michael <michael.j.feneley@gmail.com>
2016-12-10 11:57:01 -0500

    First commit. Add SALT install script

---
+++ scripts/salt_install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install salt-api
+sudo apt-get install salt-cloud
+sudo apt-get installl salt-master
+sudo apt-get install salt-minion
+sudo apt-get install salt-ssh
+sudo apt-get install salt-syndic
