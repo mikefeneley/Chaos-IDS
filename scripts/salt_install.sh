b4da22ef84d6ae47d45d5ef7e8c377095dd61520
Michael <michael.j.feneley@gmail.com>
2016-12-11 13:26:30 -0500

    Update install script

--- scripts/salt_install.sh
+++ scripts/salt_install.sh
@@ -1,6 +1,12 @@
+
 sudo apt-get install salt-api
 sudo apt-get install salt-cloud
 sudo apt-get installl salt-master
 sudo apt-get install salt-minion
 sudo apt-get install salt-ssh
 sudo apt-get install salt-syndic
+
+
+# Create top level salt directory and move all files into directory
+mkdir -p /srv/salt
+mv ../salt_conf/* /srv/salt/ 
