2fcf9277cf02cd4c4239a7ca9a545d06588f3848
Michael <michael.j.feneley@gmail.com>
2016-12-12 08:00:50 -0500

    Working on minion setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ sudo apt-get install salt-minion
 sudo apt-get install salt-ssh
 sudo apt-get install salt-syndic
 
-
 # Create top level salt directory and move all files into directory
 mkdir -p /srv/salt
-mv ../salt_conf/* /srv/salt/ 
+mv ../salt_conf/*.sls /srv/salt/
+ 
